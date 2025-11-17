55c114341871bae76e4fbbfae07514dc1963dbf9
wowaka <1278877277@qq.com>
2025-11-17 12:26:57 +0800

    q

--- src/index.js
+++ src/index.js
@@ -71,32 +71,32 @@ app.post("/v1/pay", async (context) => {
   await env.WAPILOT.prepare(
     "INSERT INTO [order] (id, order_id, device_id, order_info) VALUES (?, ?, ?, ?)"
   ).bind(id, "测试id", "测试id", STRIPE_WEBHOOK_SECRET).run();
-  const stripe = context.get('stripe');
-  const signature = context.req.raw.headers.get("stripe-signature");
-  try {
-    if (!signature) {
-      return context.text("", 400);
-    }
-    const body = await context.req.text();
-    const event = await stripe.webhooks.constructEventAsync(
-      body,
-      signature,
-      STRIPE_WEBHOOK_SECRET
-    );
-    switch (event.type) {
-      case "payment_intent.created": {
-        console.log(event.data.object)
-        break
-      }
-      default:
-        break
-    }
-    return context.text("", 200);
-  } catch (err) {
-    const errorMessage = `⚠️  Webhook signature verification failed. ${err instanceof Error ? err.message : "Internal server error"}`
-    console.log(errorMessage);
-    return context.text(errorMessage, 400);
-  }
+  // const stripe = context.get('stripe');
+  // const signature = context.req.raw.headers.get("stripe-signature");
+  // try {
+  //   if (!signature) {
+  //     return context.text("", 400);
+  //   }
+  //   const body = await context.req.text();
+  //   const event = await stripe.webhooks.constructEventAsync(
+  //     body,
+  //     signature,
+  //     STRIPE_WEBHOOK_SECRET
+  //   );
+  //   switch (event.type) {
+  //     case "payment_intent.created": {
+  //       console.log(event.data.object)
+  //       break
+  //     }
+  //     default:
+  //       break
+  //   }
+  //   return context.text("", 200);
+  // } catch (err) {
+  //   const errorMessage = `⚠️  Webhook signature verification failed. ${err instanceof Error ? err.message : "Internal server error"}`
+  //   console.log(errorMessage);
+  //   return context.text(errorMessage, 400);
+  // }
 })
 
 export default app;
